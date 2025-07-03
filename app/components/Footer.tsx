1491cd9f0bd307f2837e619d1ba69a4f912460f2
Negin <negin@greenegin.ca>
2025-07-02 20:54:10 -0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -38,8 +38,7 @@ export default function Footer({ user }: { user?: Session['user'] | null }) {
                         </Button>
                         <p className="text-green-100 dark:text-gray-300 text-sm">
                             Discover the art of the &ldquo;empty hand&rdquo;
-                            with Sensei Negin&apos;s karate classes for children ages <span>{`${siteConfig.classes.ageRange}`}</span>
-                            //4-12.
+                            with Sensei Negin&apos;s karate classes for children ages <span>{`${siteConfig.classes.ageRange}`}</span>.
                         </p>
                         {/* Justify icons: center mobile, start md+ */}
                         <div className="flex items-center justify-center md:justify-start gap-2">
