2e71d40720bb12e419ce242c85bb5d2c1d177fcf
Anton Treskunov <anton@navtrac.com>
2025-09-24 23:01:26 -0700

    dark theme support

--- app/routes/_layout.login.tsx
+++ app/routes/_layout.login.tsx
@@ -194,7 +194,7 @@ export default function LoginPage() {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
                         {heading}
                     </h2>
-                    <p className="mt-2 text-center text-sm text-gray-600">
+                    <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-300">
                         {linkPrefix}
                         {linkHref && linkLabel ? (
                             <Link to={linkHref}
