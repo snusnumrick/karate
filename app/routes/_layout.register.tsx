abeb2856ac70c4de164a11ffa91e2f74f46df940
Anton Treskunov <anton@navtrac.com>
2025-10-17 23:11:38 -0700

    infinite loop on registration page

--- app/routes/_layout.register.tsx
+++ app/routes/_layout.register.tsx
@@ -205,7 +205,7 @@ export default function RegisterPage() {
                         {/* Page Header */}
                         <div className="text-center mb-12">
                             <h1 className="text-3xl font-extrabold text-gray-900 dark:text-white sm:text-4xl">
-                                Student Registration
+                                Registration
                             </h1>
                             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 dark:text-gray-400 sm:mt-4">
                                 Join our karate family and start your martial arts journey
